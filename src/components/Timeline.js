6c04e450ffee180fbb41ae12f6e49d0e564501e2
Sushmitha V <vsushmitha10@gmail.com>
2024-07-29 18:40:59 +0530

    Reduced icon size in timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,24 +4,24 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import "./Timeline.css";
 
 function Timeline(timeline) {
-  const my_iconStyle = { background: "rgb(33, 150, 243)", color: "#fff" };
+  const my_iconStyle = {
+    background: "rgb(130, 139, 178)",
+    color: "#fff",
+    width: "40px",
+    height: "40px",
+  };
   const timelineItems = timeline.timeline;
   console.log(timelineItems);
   return (
-    <VerticalTimeline
-      className="border" //layout="1-column-left"
-    >
+    <VerticalTimeline>
       {timelineItems.map((entry) => (
         <VerticalTimelineElement
           className="vertical-timeline-element--work"
-          // contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          // contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
           date={entry.date}
           iconStyle={my_iconStyle}
-          // icon={<WorkIcon />}
-          // position="right"
         >
           <h3 className="vertical-timeline-element-title">{entry.title}</h3>
           <h2 className="vertical-timeline-element-subtitle">
